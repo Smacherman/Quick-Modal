43bd511e43145f21612b6324a16030b8e55e5461
Ryan Moore <Smacherman@gmail.com>
2021-03-27 16:56:38 -0700

    Added in basic Javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Open button
+document.getElementById("open-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "block";
+})
+
+
+// Close Button
+document.getElementById("close-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+})
